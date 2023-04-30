5bbda8ae2b9859d21ad789e51d9ca55fd17d4485
JEONGDratini <pw050503@gmail.com>
2023-04-30 14:13:04 +0900

    update image

--- routes/root.js
+++ routes/root.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: true, host: process.env.MONGO_URL, username: process.env.MONGO_INITDB_ROOT_USERNAME }
+    return { root: true, host: process.env.MONGO_HOSTNAME, username: process.env.MONGO_USERNAME }
   })
 } 
