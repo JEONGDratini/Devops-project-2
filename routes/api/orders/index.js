0df1f39f0ef4de0bb340f1ef5d6efe4580626153
JEONGDratini <pw050503@gmail.com>
2023-05-02 13:48:04 +0900

    try post method MILESTONE10

--- routes/api/orders/index.js
+++ routes/api/orders/index.js
@@ -25,98 +25,61 @@ module.exports = async function (fastify, opts) {
     }
   })
 
-//   fastify.get('/:orderid', async function (req, reply) {
+  fastify.get('/:orderid', async function (req, reply) {
 
-//     const client = await MongoClient.connect(url, { useUnifiedTopology: true });
-//     const db = client.db(dbName);
-//     const collection = db.collection(collectionName);
+    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
 
-//     const query = {_id: new ObjectId(req.params.orderid)};
+    const query = {_id: new ObjectId(req.params.orderid)};
 
 
-//     const result = await collection.findOne(query);
+    const result = await collection.findOne(query);
 
-//     client.close();
+    client.close();
     
-//     reply.code(200).send(result);
-//   })
-
-//   fastify.post('/', async function (req, reply) {
-//     const { restaurantId, menu } = req.body;
-//     console.log(restaurantId);
-//     const client = await MongoClient.connect(url, { useUnifiedTopology: true });
-//     const db = client.db(dbName);
-
-//     //배달부 정보 받아오기
-//     const courier_collection = db.collection('courier');
-
-//     const courier_query={ available: false }
-//     const courier = await courier_collection.findOne(courier_query)
-//     console.log(courier);
-//     //식당 정보 받아오기
-//     const restaurant_collection = db.collection('restaurants');
-//     const restaurant_query = {_id: `${restaurantId}`};
-
-//     const restaurant = await restaurant_collection.findOne(restaurant_query);
-//     console.log(restaurant);
-
-//     //주문정보 insert하기
-//     const order_collection = db.collection(collectionName);
-//     const order_query ={
-//         deliveryInfo: {
-//             status: 'preparing',
-//             assignedCourier: '박배송',
-//             estimatedDeleveryTime: 40
-//         },
-//         consumer_id: new ObjectId('64504e3a6890c3c570872978'),
-//         restaurant: restaurant,
-//         orderedMenu: menu
-        
-        
-//     }
-
-//     const result = await order_collection.insertOne(order_query)
-
-//     client.close();
-    
-//     reply.code(201).send(result);
-//   })
+    reply.code(200).send(result);
+  })
 
   fastify.post('/', async function (req, reply) {
-    try {
-      const { restaurantId, menu } = req.body
-      console.log(req.body)
+    const { restaurantId, menu } = req.body;
+    console.log(restaurantId);
+    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+    const db = client.db(dbName);
 
-      const restaurants = await this.mongo.db.collection('restaurants')
-      const findRestaurant = await restaurants.findOne({_id: new ObjectId(restaurantId)});
-      console.log('findRestaurant : ', findRestaurant)
+    //배달부 정보 받아오기
+    const courier_collection = db.collection('courier');
 
-      const response = {
-        restaurant: {
-          _id: restaurantId,
-          name: findRestaurant.name,
-          menu: findRestaurant.menu,
-          address: findRestaurant.address,
-          rating: findRestaurant.rating
-        },
-        orderedMenu: menu,
+    const courier_query={ available: false }
+    const courier = await courier_collection.findOne(courier_query)
+    console.log(courier);
+    //식당 정보 받아오기
+    const restaurant_collection = db.collection('restaurants');
+    const restaurant_query = {_id: `${restaurantId}`};
+
+    const restaurant = await restaurant_collection.findOne(restaurant_query);
+    console.log(restaurant);
+
+    //주문정보 insert하기
+    const order_collection = db.collection(collectionName);
+    const order_query ={
         deliveryInfo: {
-          status: 'PREPARING',
-          assignedCourier: 'Kim Baedal',
-          estimatedDeleveryTime: 40
-        }
-      }
-      console.log('response : ', response)
-      const order = await this.mongo.db.collection('order')
-      const one = await order.insertOne(response);
-      console.log(`one :`, one);
-
-      const findOrder = await order.findOne({_id: new ObjectId(one.insertedId)});
-      
-      return findOrder;
-    } catch (err) {
-      return `FAIDED HI ${err}`
+            status: 'preparing',
+            assignedCourier: '박배송',
+            estimatedDeleveryTime: 40
+        },
+        consumer_id: new ObjectId('64504e3a6890c3c570872978'),
+        restaurant: restaurant,
+        orderedMenu: menu
+        
+        
     }
+
+    const result = await order_collection.insertOne(order_query)
+
+    client.close();
+    
+    reply.code(201).send(result);
   })
 
 }
